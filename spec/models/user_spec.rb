b9a5fe074f506b8e26a75ead54b31ebfa84288ac
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-03-25 23:12:40 +0900

    Userのvalidationテストの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,13 +24,37 @@ RSpec.describe User, type: :model do
   end
 
   # 姓がなけれな無効な状態であること
-  it "is invalid without a last name"
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
 
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address"
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Summer',
+      email: 'tester@example.com',
+      password: 'dottle_nouveau-pavilion-tights-furze',
+    )
+
+    user = User.new(
+      first_name: 'Aaron',
+      last_name: 'Summer',
+      email: 'tester@example.com',
+      password: 'dottle_nouveau-pavilion-tights-furze',
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 
   # ユーザーのフルネームを文字列として返すこと
   it "return a user's full name as a string"
