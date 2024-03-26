651bd0d70a82723a67ae7ff268deb40ef00d0541
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-03-27 00:24:01 +0900

    factory_botの実装確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  # 有効なファクトリを持つこと
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
     user = User.new(
