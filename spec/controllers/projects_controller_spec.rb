1ec226e07f69f3fe71a8cbf8c3c4b116c5f8592c
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-03-28 23:43:34 +0900

    updateのテスト作成

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -105,4 +105,70 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe 'update' do
+    # 認可されたユーザーとして
+    context 'as an authorized user' do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # プロジェクトを更新できること
+      it 'updates a project' do
+        project_params = FactoryBot.attributes_for(:project, name: "New Project Name")
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        #  reload を使わないと、メモリに保存された値が使われてしまう
+        expect(@project.reload.name).to eq 'New Project Name'
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context 'as an unauthorized user' do
+      before  do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user, name: 'Same Old Name')
+      end
+
+      #プロジェクトを更新できないこと
+      it 'does not update the project' do
+        project_params = FactoryBot.attributes_for(:project, name: 'New name')
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(@project.reload.name).to eq 'Same Old Name'
+      end
+
+      # ダッシュボードへリダイレクトすること
+      it 'redirect to the dashboard' do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    # ゲストとして
+    context 'as a guest' do
+      before do
+        @project = FactoryBot.create(:project)
+      end
+
+      # 302レスポンスを返すこと
+      it 'returns a 302 response' do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to have_http_status '302'
+      end
+
+      # サインイン画面にリダイレクトすること
+      it 'redirect to the sign-in page' do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to redirect_to '/users/sign_in'
+      end
+
+    end
+  end
 end
