880f423aa91828d4ba33878a49951c533ba8b7a8
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-04-06 14:59:20 +0900

    note specをletを使って書き換え

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,27 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-
-  before do
-    # このファイルの全テストで使用するテストデータをセットアップする
-    @user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
-    )
-
-    @project = @user.projects.create(
-      name: 'Test Project'
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it 'is valid with a user, project, and message' do
     note = Note.new(
       message: 'This is a sample note.',
-      user: @user,
-      project: @project
+      user: user,
+      project: project
     )
     expect(note).to be_valid
   end
@@ -36,29 +24,38 @@ RSpec.describe Note, type: :model do
   # 文字列に一致するメッセージを検索する: describe にはクラスやシステムの機能に関するアウトラインを記述
   describe 'search message for a term' do
 
-    before do # before :each と同じで、各テストの前に実行される
-      # 検索機能の全テストに関連する追加のテストデータをセットアップする
-      @note1 = @project.notes.create(
-        message: 'This is the first note.',
-        user: @user
+    let(:note1) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the first note.'
       )
+    }
 
-      @note2 = @project.notes.create(
-        message: 'This is the second note.',
-        user: @user
+    let(:note2) {
+      Factorybot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the second note.'
       )
+    }
 
-      @note3 = @project.notes.create(
-        message: 'First, preheat the oven.',
-        user: @user
+    let(:note3) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'First, preheat the oven.'
       )
-    end
+    }
 
     # 一致するデータが見つかるとき: contextには特定の状態に関するアウトラインを記述
     context 'when a match is found' do
       # 検索文字列に一致するメモを返すこと
       it 'returns notes that math the search term' do
-        expect(Note.search('first')).to include(@note1, @note3)
+        expect(Note.search('first')).to include(note1, note3)
       end
     end
 
